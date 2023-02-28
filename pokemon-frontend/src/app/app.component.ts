f8924c1b3104402280d9d8bb57cae4de7b37e53d
Colin Quelle <cmq1999@yahoo.com>
2023-02-27 23:02:05 -0500

    CSS fixes, added animated Pokedex description

--- pokemon-frontend/src/app/app.component.ts
+++ pokemon-frontend/src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit, OnDestroy{
   pokemonName: String = "";
   pokemonImage: String = "";
   pokemonPokedexDesc: String = "";
+  animatedPokedexDesc: String = "";
+  typeAnimationInterval: any;
 
   subscriptionList: Subscription[] = [];
 
@@ -25,24 +27,48 @@ export class AppComponent implements OnInit, OnDestroy{
   onEnter(){
     this.pokemonImage = "";
     this.pokemonPokedexDesc = "";
+    this.animatedPokedexDesc = "";
+    if(this.typeAnimationInterval != undefined){
+      clearInterval(this.typeAnimationInterval);
+    }
+    //get pokemon sprite
     this.subscriptionList.push(this.pokeApiService.getPokemonInfo(this.pokemonName.trim().toLowerCase()).subscribe(
       (data) => {
-        this.pokemonImage = data.sprites.front_default;
-        console.log(data);
-        this.subscriptionList.push(this.pokeApiService.getSpeciesInfo(data.species.url).subscribe((speciesData) => {
-          var pokedex_entry = speciesData.flavor_text_entries.find((entry) => entry.language['name'] == 'en' && entry.version['name'] == 'red');
-          this.pokemonPokedexDesc = pokedex_entry?.flavor_text!;
-          console.log(this.pokemonPokedexDesc);
-        }));
+        if(data){
+          this.pokemonImage = data.sprites.front_default;
+
+          //get pokemon pokedex description
+          this.subscriptionList.push(this.pokeApiService.getSpeciesInfo(data.species.url).subscribe((speciesData) => {
+            var pokedex_entry = speciesData.flavor_text_entries.find((entry) => entry.language['name'] == 'en' && entry.version['name'] == 'red');
+            this.pokemonPokedexDesc = pokedex_entry?.flavor_text!;
+            this.pokemonPokedexDesc = this.pokemonPokedexDesc.replace('\f', ' ');
+
+            //start type animation for pokedex description
+            this.typeAnimation();
+          }));
+
+        }
       },
       (err: Error) => {}
     ));
+  }
+
+  typeAnimation(){
+    var charIndex = 0;
+    this.typeAnimationInterval = setInterval(() => {
+      this.animatedPokedexDesc += this.pokemonPokedexDesc[charIndex];
+      charIndex += 1;
+      if(charIndex >= this.pokemonPokedexDesc.length){
+        clearInterval(this.typeAnimationInterval);
+      }
+    }, 50);
   }
 
   ngOnDestroy(): void {
     for(let sub of this.subscriptionList){
       sub.unsubscribe();
     }
+    clearInterval(this.typeAnimationInterval);
   }
 
 }
